f6dce5a1be675545a5d9a2c1e98d6c28fe360e6b
Dedi Prakasa <dedprakasa@gmail.com>
2021-04-04 14:09:31 +0700

    initial commit

---
+++ src/main/java/com/deltadirac/sandbox/SpringBootSandboxApplication.java
@@ -0,0 +1,13 @@
+package com.deltadirac.sandbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSandboxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootSandboxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/deltadirac/sandbox/SpringBootSandboxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deltadirac.sandbox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootSandboxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
